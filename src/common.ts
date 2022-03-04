0a52d75bda8a41de7d77b2f3a545217f63ef2a29
Brendon Dulam <brendondulam06@gmail.com>
2022-03-04 20:02:39 +0900

    camel case

--- src/common.ts
+++ src/common.ts
@@ -2,8 +2,8 @@ export const unEntity = (str: string) => {
   return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
 }
 
-export function camelCase(str: string) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+export const camelCase = (str: string) => {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => {
     if (+match === 0) return ''
     return index === 0 ? match.toLowerCase() : match.toUpperCase()
   })
